e16264c69a21abab6d0cf7bb93c91feab6670593
mkunaparaju <mkunaparaju@gmail.com>
2018-02-25 11:43:18 -0700

    Week 1 completed

--- AlgorithmicToolbox/src/Week1/APlusB.java
+++ AlgorithmicToolbox/src/Week1/APlusB.java
@@ -2,10 +2,12 @@ package Week1;
 
 import java.util.Scanner;
 class APlusB {
+
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     int a = s.nextInt();
     int b = s.nextInt();
     System.out.println(a + b);
   }
+
 }
--- AlgorithmicToolbox/src/Week1/MaxPairwiseProduct.java
+++ AlgorithmicToolbox/src/Week1/MaxPairwiseProduct.java
@@ -4,30 +4,63 @@ import java.io.*;
 import java.util.*;
 
 public class MaxPairwiseProduct {
-    static int getMaxPairwiseProduct(int[] numbers) {
-       int product = 0;
-       int maxHigh = 0;
-       int maxLow = 0;
-
-       for(int i = 0; i < numbers.length; i++)  {
-           if(numbers[i] >= maxHigh) maxHigh = numbers[i];
+    static long getMaxPairwiseProduct(int[] numbers) {
+       long product = 0;
+       long maxHigh = 0;
+       long maxLow = 0;
+        int highIndex = 0;
+       for(int i = 0; i < numbers.length; i++) {
+           if (numbers[i] >= maxHigh) {
+               maxHigh = numbers[i];
+               highIndex = i;
+           }
        }
-
-
         for(int i = 0; i < numbers.length; i++)  {
-            if(numbers[i] >= maxLow && maxHigh != maxLow) maxLow = numbers[i];
+            if(numbers[i] >= maxLow && highIndex!= i) maxLow = numbers[i];
         }
+        return maxHigh*maxLow;
+    }
 
-        return maxHigh*maxHigh;
+    static long getMaxPairwiseNaive(int [] numbers )    {
+        int result = 0;
+        int n = numbers.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (numbers[i] * numbers[j] > result) {
+                    result = numbers[i] * numbers[j];
+                }
+            }
+        }
+        return result;
+    }
+
+    static void stressTest(int n, int m)    {
+       while(true) {
+           int random = (int) (Math.random() * n + 1);
+           int[] testArr = new int[random];
+           for (int i = 0; i < random; i++) {
+               testArr[i] = (int) (Math.random() * m + 1);
+           }
+           long r1 = getMaxPairwiseNaive(testArr);
+           long r2 = getMaxPairwiseProduct(testArr);
+
+           if (r1 == r2)
+               System.out.println("OK");
+           else {
+               System.out.println("Wrong answer " + r1 + " " + r2);
+               return;
+           }
+       }
     }
     public static void main(String[] args) {
-        FastScanner scanner = new FastScanner(System.in);
-        int n = scanner.nextInt();
-        int[] numbers = new int[n];
-        for (int i = 0; i < n; i++) {
-            numbers[i] = scanner.nextInt();
-        }
-        System.out.println(getMaxPairwiseProduct(numbers));
+//        FastScanner scanner = new FastScanner(System.in);
+//        int n = scanner.nextInt();
+//        int[] numbers = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            numbers[i] = scanner.nextInt();
+//        }
+        stressTest(10, 10000);
+        //System.out.println(getMaxPairwiseProduct(numbers));
     }
     static class FastScanner {
         BufferedReader br;
